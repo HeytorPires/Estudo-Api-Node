8793c26a9c7c798fc4ca41a3fd29c23fc53c80f6
HeytorPires <heytor.cacho29@gmail.com>
2024-08-17 01:48:09 -0300

    :Construction: Brics: Rotas

---
+++ src/server/routes/index.ts
@@ -0,0 +1,12 @@
+import { Router } from "express";
+import { StatusCodes } from "http-status-codes";
+const router = Router();
+
+router.get("/", (_, res) => {
+    return res.send("olá, dev!");
+});
+router.post("/teste", (req, res) => {
+    console.log(req.body);
+    return res.status(StatusCodes.UNAUTHORIZED).json(req.body);
+});
+export { router };
